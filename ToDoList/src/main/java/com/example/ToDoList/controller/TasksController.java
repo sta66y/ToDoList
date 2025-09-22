a8960953a3a8d502e1f333848061823c6de70d23
sta66y <sta66y@yahoo.com>
2025-09-22 17:27:56 +0700

    controller done

--- ToDoList/src/main/java/com/example/ToDoList/controller/TasksController.java
+++ ToDoList/src/main/java/com/example/ToDoList/controller/TasksController.java
@@ -12,5 +12,40 @@ import java.util.List;
 @RequestMapping("api/todos")
 @AllArgsConstructor
 public class TasksController {
+    private final TasksService tasksService;
 
+    @GetMapping
+    public List<TaskResponseDTO> getAllTasks() {
+        return tasksService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public TaskResponseDTO getTaskById(@PathVariable Long id) {
+        return tasksService.getTaskById(id);
+    }
+
+    @PostMapping
+    public TaskResponseDTO createTask(@RequestBody TaskRequestDTO dto) {
+        return tasksService.createTask(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public TaskResponseDTO deleteTaskById(@PathVariable Long id) {
+        return tasksService.deleteTaskById(id);
+    }
+
+    @PutMapping("/{id}")
+    public TaskResponseDTO editTaskById(@PathVariable Long id, @RequestBody TaskRequestDTO dto) {
+        return tasksService.editTaskById(id, dto);
+    }
+
+    @PatchMapping("/{id}")
+    public TaskResponseDTO changeCompletedStatusById(@PathVariable Long id) {
+        return tasksService.changeCompletedStatusById(id);
+    }
+
+    @PostMapping("/upload")
+    public List<TaskResponseDTO> uploadTasksFromJson(@RequestBody List<TaskRequestDTO> dtos) {
+        return tasksService.uploadTasksFromJson(dtos);
+    }
 }
