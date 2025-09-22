9bb11effde4f41adbf821f0b8324d95ee68ad298
sta66y <sta66y@yahoo.com>
2025-09-22 21:27:14 +0700

    added GlobalExceptionHandler

--- ToDoList/src/main/java/com/example/ToDoList/controller/GlobalExceptionHandler.java
+++ ToDoList/src/main/java/com/example/ToDoList/controller/GlobalExceptionHandler.java
@@ -1,4 +1,33 @@
 package com.example.ToDoList.controller;
 
+import com.example.ToDoList.exceptions.TaskNotFound;
+import com.example.ToDoList.models.AppError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(TaskNotFound.class)
+    public ResponseEntity<AppError> handleTaskNotFound(TaskNotFound ex) {
+        return new ResponseEntity<>(new AppError(ex.getMessage(), HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<AppError> methodArgumentNotValid(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .findFirst()
+                .orElse("Invalid request");
+
+        return new ResponseEntity<>(
+                new AppError(errorMessage, HttpStatus.BAD_REQUEST.value()),
+                HttpStatus.BAD_REQUEST
+        );
+    }
 }
