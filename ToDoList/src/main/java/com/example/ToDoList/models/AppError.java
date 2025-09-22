0a5928ed8b6bb00dab7e7ac1939da06307f42eec
sta66y <sta66y@yahoo.com>
2025-09-22 21:26:50 +0700

    added AppError

--- ToDoList/src/main/java/com/example/ToDoList/models/AppError.java
+++ ToDoList/src/main/java/com/example/ToDoList/models/AppError.java
@@ -1,4 +1,11 @@
 package com.example.ToDoList.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class AppError {
+    private String message;
+    private int code;
 }
